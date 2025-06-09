5eb44e87ed4f3e751f6a17320832172879a8830c
tarla <Tarlavina50@mail.ru>
2025-06-09 21:39:04 +0700

    Классная работа (итог)

--- Assets/Data/Scripts/Scene3/CubeScene3.cs
+++ Assets/Data/Scripts/Scene3/CubeScene3.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class CubeScene3 : MonoBehaviour
 {
-
+    private Rigidbody _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    public void PrepereDrag()
+    {
+        _rigidbody.useGravity = false;
+        _rigidbody.isKinematic = true;
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+    public void PrepereDrop()
+    {
+        _rigidbody.useGravity = true;
+        _rigidbody.isKinematic = false;
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+    }
 }
--- Assets/Data/Scripts/Scene3/Interactedle.cs
+++ Assets/Data/Scripts/Scene3/Interactedle.cs
@@ -5,9 +5,67 @@ public class Interact : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private int _sizeAim;
     [SerializeField] private string _aimChar;
+    [SerializeField] private Transform _startPointRay;
+    [SerializeField] private float _rayDistance;
+    [SerializeField] private Transform _itemPosition;
+
 
     private float _aimPositionDrawX;
     private float _aimPositionDrawY;
+    private RaycastHit _raycastHit;
+    private Ray _ray;
+    private bool _isHaveItem = false;
+    private CubeScene3 _tempCube;
+
+    private void Update()
+    {
+        CastRay();
+        TakeItem();
+    }
+    private void CastRay()
+    {
+        _ray = new Ray(_startPointRay.position, _camera.transform.forward);
+
+        Debug.DrawRay(_ray.origin, _ray.direction * _rayDistance, Color.red);
+    }
+    private void TakeItem()
+    {
+        if (Input.GetMouseButtonDown(0) && _isHaveItem == false)
+        {
+            if (Physics.Raycast(_ray, out _raycastHit, _rayDistance))
+            {
+                //if (_raycastHit.transform.TryGetComponent<CubeScene3>(out CubeScene3 cube))
+                if (_raycastHit.transform.TryGetComponent<CubeScene3>(out CubeScene3 cube))
+                {
+                    //_nameObj.text = cube.Name; // подготовка куда к взятию
+                    _tempCube = cube;
+                    _tempCube.PrepereDrag();
+                    Drag();
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(0) && _isHaveItem == true)
+        {
+            {
+                Drop();
+            }
+        }
+    }
+
+    private void Drop()
+    {
+        _isHaveItem = false;
+        _tempCube.PrepereDrop();
+        _tempCube.transform.SetParent(null);
+        _tempCube = null;
+    }
+
+    private void Drag()
+    {
+        _isHaveItem = true;
+        _tempCube.transform.position = _itemPosition.position;
+        _tempCube.transform.SetParent(this.transform);
+    }
 
     private void OnGUI()
     {
